725469686ddcbc46767eef09d2669159ce2f51d8
Ben Ritter <benjaminaritter@gmail.com>
2018-08-20 00:02:49 +0800

    Fix building inheritence bug

--- src/svg/Building.jsx
+++ src/svg/Building.jsx
@@ -6,20 +6,20 @@ import Icon from './Icon';
 
 import pawn from '../assets/icons/lorc/originals/svg/000000/transparent/pawn.svg';
 
+const BuildingSvg  = gridPositioned(({ name, icon, ...rest }) => (
+  <g className="building" {...rest}>
+    <Text className="debug">{name || 'building'}</Text>
+    <Icon asset={icon} />
+  </g>
+));
+
 class Building extends React.PureComponent {
   static icon = pawn;
 
   render() {
-    const { ...rest } = this.props;
     const { name, icon } = this.constructor;
-
-    return (
-      <g className="building" {...rest}>
-        <Text className="debug">{name || 'building'}</Text>
-        <Icon asset={icon} />
-      </g>
-    );
+    return <BuildingSvg {...{ name, icon, ...this.props }} />;
   }
 }
 
-export default gridPositioned(Building);
+export default Building;
